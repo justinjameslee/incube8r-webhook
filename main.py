24317697a4165c70e196c5c8eeedccb5da26c701
Justin Lee <justinjameslee5@gmail.com>
2024-11-01 17:15:46 +1100

    refactor to support json format

--- main.py
+++ main.py
@@ -4,68 +4,66 @@ from datetime import datetime
 import gspread
 from google.oauth2.service_account import Credentials
 
-# Get input data from environment variables
-text = os.getenv("INPUT_DATA", "")
-date_text = os.getenv("DATE_DATA", "")
-google_sheet_name = os.getenv("GOOGLE_SHEET_NAME")  # Name of your Google Sheet
-service_account_file = os.getenv("SERVICE_ACCOUNT_FILE")  # Path to Google service account JSON file
-
-print(text)
-print(date_text)
-
-# Initialize variables for processing
-output = {
-    'product_names': [],
-    'artist_names': [],
-    'quantities': [],
-    'prices': [],
-    'categories': [],
-    'date_ddmmyyyy': [],
-    'time_hhmmss': [],
-    'month_year': []
-}
-
-
-# Parse date
-date_text = date_text.strip()
-date_parsed = datetime.strptime(date_text, '%Y-%m-%dT%H:%M:%S.%fZ')
-
-# Process email content
-idx = text.find("Product Quantity Price\\n")
-idx += len("Product Quantity Price\\n")
-text = text[idx:]
-
-pattern = r'(.+?)\s+by\s+(.*?)\s*\(#\d+\)\s+(\d+)\s+\$(\d+\.\d{2})'
-for match in re.findall(pattern, text):
-    output["product_names"].append(match[0].strip())
-    output["artist_names"].append(match[1].strip())
-    output["quantities"].append(match[2])
-    output["prices"].append(match[3])
-    output["categories"].append('=IFERROR(VLOOKUP(INDIRECT("A"&ROW()), Categories!A:B, 2, FALSE), "Other")')
-    output["date_ddmmyyyy"].append(date_parsed.strftime('%d/%m/%Y'))
-    output["time_hhmmss"].append(date_parsed.strftime('%H:%M:%S'))
-    output["month_year"].append(date_parsed.strftime('%m/%y'))
-
-# Google Sheets API Setup
+# Setup Google Sheets access
+service_account_file = os.getenv("SERVICE_ACCOUNT_FILE")
+google_sheet_name = os.getenv("GOOGLE_SHEET_NAME")
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
-
 credentials = Credentials.from_service_account_file(service_account_file, scopes=SCOPES)
 gc = gspread.authorize(credentials)
-
 spreadsheet = gc.open(google_sheet_name)
 worksheet = spreadsheet.worksheet("Data")
 
-# Prepare data to insert into Google Sheets
+# Get input data from environment variables
+order_text = os.getenv("ORDER_DATA", "")
+date_text = os.getenv("DATE_DATA", "")
+input_text = os.getenv("INPUT_DATA", "")
+
+# Debug
+print(order_text)
+print(input_text)
+print(date_text)
+
+# Initialize output dictionary
+output = {
+    "order_num": [],
+    "product_names": [],
+    "artist_names": [],
+    "quantities": [],
+    "prices": [],
+    "categories": [],
+    "date_ddmmyyyy": [],
+    "time_hhmmss": [],
+    "month_year": []
+}
+
+# Parse the date in the given format
+date_parsed = datetime.strptime(date_text.strip(), '%Y-%m-%dT%H:%M:%S.%fZ')
+
+for sale_details in input_text:
+    product_artist = re.search(r"^(.*?)\s+by\s+([^\(]+)", sale_details['product'])
+
+    # Append extracted data to output lists
+    output["order_num"].append(order_text)
+    output["product_names"].append(product_artist.group(1).strip())
+    output["artist_names"].append(product_artist.group(2).strip())
+    output["quantities"].append(sale_details['quantity'])
+    output["prices"].append(re.sub(r'[^\d.]', '', sale_details["price"]))
+    output["categories"].append('=IFERROR(VLOOKUP(INDIRECT("A"&ROW()), Categories!A:B, 2, FALSE), "Other")')
+    output["date_ddmmyyyy"].append(date_parsed.strftime('%d/%m/%Y'))
+    output["time_hhmmss"].append(date_parsed.strftime('%H:%M:%S'))
+    output["month_year"].append(date_parsed.strftime('%m/%Y'))
+
+# Prepare data for batch update
 data_rows = [
-    [output['product_names'][i], output['categories'][i], output['quantities'][i], output['prices'][i], 
-     output['date_ddmmyyyy'][i], output['time_hhmmss'][i], output['month_year'][i]]
+    [output['order_num'][i], output['product_names'][i], output['artist_names'][i], output['quantities'][i], output['prices'][i], 
+     output['categories'][i], output['date_ddmmyyyy'][i], output['time_hhmmss'][i], output['month_year'][i]]
     for i in range(len(output['product_names']))
 ]
 
 # Find the next available row
 next_row = len(worksheet.get_all_values()) + 1
 
-# Batch update to avoid single quotes in Google Sheets
+# Batch update to Google Sheets
 cell_range = f"A{next_row}:G{next_row + len(data_rows) - 1}"
 worksheet.batch_update([{
     "range": cell_range,
